bb43663ac98a400c273a1880ca7c44dad61fdeb6
uBBeterDS <sgrigorij142@gmail.com>
2026-04-20 13:08:15 +0300

    Auto-save: update lightgcn.py

--- src/models/lightgcn.py
+++ src/models/lightgcn.py
@@ -71,6 +71,7 @@ class LightGCN(nn.Module, BaseRecommender):
         device = torch.device(device)
         self.to(device)
         graph = graph.to(device)
+        self._graph = graph
 
         optimizer = torch.optim.Adam(self.parameters(), lr=lr)
         train = dataset.train
